e1f24b1c68cc2d9769cc1b2c462f38a07a650a22
FasihLisan <fasihullisan091966@gmail.com>
2024-08-06 11:25:05 +0700

    Slicing bottomNavigationMenu component & custom themes color

--- app/src/main/java/com/otakkanan/taskapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/otakkanan/taskapp/ui/main/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
+        navView.itemActiveIndicatorColor
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
