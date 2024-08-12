7efeb1d77bf90b1bf428da0e1028eadb6775c9b4
FasihLisan <fasihullisan091966@gmail.com>
2024-08-12 18:26:46 +0700

    [Done] Detail Tugas & Anggota Tim Page

--- app/src/main/java/com/otakkanan/taskapp/ui/main/beranda/anggota/list_anggota/ListAnggotaAdapter.kt
+++ app/src/main/java/com/otakkanan/taskapp/ui/main/beranda/anggota/list_anggota/ListAnggotaAdapter.kt
@@ -1,4 +1,4 @@
-package com.otakkanan.taskapp.ui.main.beranda.detail_task
+package com.otakkanan.taskapp.ui.main.beranda.anggota.list_anggota
 
 import android.app.Activity
 import android.content.Intent
@@ -20,40 +20,52 @@ import com.bumptech.glide.Glide
 import com.otakkanan.taskapp.R
 import com.otakkanan.taskapp.data.model.Team
 import com.otakkanan.taskapp.databinding.AnggotaItemBinding
+import com.otakkanan.taskapp.databinding.AnggotaListItemBinding
 import com.otakkanan.taskapp.databinding.SubTugasListBinding
 import com.otakkanan.taskapp.ui.main.beranda.anggota.list_anggota.ListAnggotaActivity
 
-class AnggotaAdapter(private val onAddClick: (Team) -> Unit) :  ListAdapter<Team, AnggotaAdapter.MyViewHolder>(DIFF_CALLBACK) {
+class ListAnggotaAdapter :  ListAdapter<Team, ListAnggotaAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding = AnggotaItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = AnggotaListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val items = getItem(position)
         val itemCount = currentList.size
-        holder.bind(items,position, itemCount,onAddClick)
+        holder.bind(items,position, itemCount)
     }
 
-    class MyViewHolder(private val binding: AnggotaItemBinding): RecyclerView.ViewHolder(binding
+    class MyViewHolder(private val binding: AnggotaListItemBinding): RecyclerView.ViewHolder(binding
         .root) {
 
-        fun bind(items: Team,position: Int,size: Int,onAddClick: (Team) -> Unit){
+        fun bind(items: Team,position: Int,size: Int){
             with(binding){
-                if (position != 0){
-                    itemView.translationX = (-40*position).toFloat()
-                }
-                if (position == size-1){
-                    btnContainer.visibility = View.VISIBLE
-                }
-                btnTambah.setOnClickListener{
-                    onAddClick(items)
-                }
-                imageAnggota.visibility = View.VISIBLE
                 Glide.with(itemView.context)
                     .load(items.image)
-                    .into(imageAnggota)
+                    .into(image)
+                title.text = items.name
+                when(items.role){
+                    "Admin" -> {
+                        role.text = items.role
+                        role.setTextColor(ContextCompat.getColor(itemView.context, R.color
+                            .md_theme_primary))
+                        roleContainer.setCardBackgroundColor(ContextCompat.getColor(itemView.context, R.color
+                            .colorPurpleContainer))
+                    }
+                    "Manager" -> {
+                        role.text = items.role
+                        role.setTextColor(ContextCompat.getColor(itemView.context, R.color
+                            .colorGold))
+                        roleContainer.setCardBackgroundColor(ContextCompat.getColor(itemView.context, R.color
+                            .colorGoldContainer))
+                    }
+                    else -> {
+                        roleContainer.visibility = View.GONE
+                    }
+                }
+
             }
         }
 
