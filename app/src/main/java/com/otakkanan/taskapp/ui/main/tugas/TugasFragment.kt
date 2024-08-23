9bf29f5dfc182a35fc4a3d96628703c97609f510
Marzuki Akmal <marzukiakmal1@gmail.com>
2024-08-23 09:35:23 +0700

    [Update] Delete Item Feature Tab Tugas in List Tugas Screen
    Swipe For Delete

--- app/src/main/java/com/otakkanan/taskapp/ui/main/tugas/TugasFragment.kt
+++ app/src/main/java/com/otakkanan/taskapp/ui/main/tugas/TugasFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.otakkanan.taskapp.R
 import com.otakkanan.taskapp.data.model.TaskDay
 import dagger.hilt.android.AndroidEntryPoint
@@ -45,6 +47,37 @@ class TugasFragment : Fragment(), TugasAdapter.OnTaskStatusChangeListener {
 
         // Display all tasks, regardless of priority or status
         adapter.updateTasks(allTasks)
+
+        // Implement swipe-to-dismiss for task deletion
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // No move operation needed, return false
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val taskToRemove = allTasks[position]
+
+                // Remove the task from the list
+                allTasks.removeAt(position)
+                adapter.notifyItemRemoved(position)
+
+                // Optionally, show an undo option using Snackbar
+                Snackbar.make(recyclerView, "${taskToRemove.title} deleted", Snackbar.LENGTH_LONG)
+                    .setAction("Undo") {
+                        // Reinsert the task if the user undoes the delete
+                        allTasks.add(position, taskToRemove)
+                        adapter.notifyItemInserted(position)
+                    }.show()
+            }
+        })
+
+        itemTouchHelper.attachToRecyclerView(recyclerView)
 
         return view
     }
