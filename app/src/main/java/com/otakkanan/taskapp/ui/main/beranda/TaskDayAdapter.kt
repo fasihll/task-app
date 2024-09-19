6bf9f8754b409f4f4e27868e8d3efa207d6868c8
FasihLisan <fasihullisan091966@gmail.com>
2024-09-19 14:53:26 +0700

    [Bug Ui Fix] dark theme in dashboard and calender page

--- app/src/main/java/com/otakkanan/taskapp/ui/main/beranda/TaskDayAdapter.kt
+++ app/src/main/java/com/otakkanan/taskapp/ui/main/beranda/TaskDayAdapter.kt
@@ -1,6 +1,8 @@
 package com.otakkanan.taskapp.ui.main.beranda
 
+import android.content.res.Resources.Theme
 import android.graphics.Paint
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -8,8 +10,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.otakkanan.taskapp.R
+import com.google.android.material.R as Rmat
 import com.otakkanan.taskapp.data.model.TaskDay
 import com.otakkanan.taskapp.databinding.TaskDayListBinding
+
 
 class TaskDayAdapter :  ListAdapter<TaskDay, TaskDayAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
@@ -54,20 +58,42 @@ class TaskDayAdapter :  ListAdapter<TaskDay, TaskDayAdapter.MyViewHolder>(DIFF_C
         }
 
         private fun setCheckedState() {
+            var theme: Theme = itemView.context.getTheme()
+            var tV = TypedValue()
+
             binding.taskTitle.paintFlags = binding.taskTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
             binding.taskCheckbox.setImageResource(R.drawable.unchecked_task)
             binding.taskTitle.setTextColor(ContextCompat.getColor(itemView.context, R.color.md_theme_onBackground))
-            binding.taskTime.setTextColor(ContextCompat.getColor(itemView.context, R.color.md_theme_primary))
-            binding.taskTimeContainer.setCardBackgroundColor(ContextCompat.getColor(itemView.context, R.color.md_theme_primaryFixed))
+
+            var colorPrimaryContainer = theme.resolveAttribute(Rmat.attr.colorPrimaryContainer, tV,
+                true)
+            var taskTime: Int = tV.data
+            binding.taskTime.setTextColor(taskTime)
+            var colorPrimaryFixed = theme.resolveAttribute(Rmat.attr.colorPrimaryFixed, tV,
+                true)
+            var taskTimeContainer: Int = tV.data
+            binding.taskTimeContainer.setCardBackgroundColor(taskTimeContainer)
+
+
         }
 
         private fun setUncheckedState() {
+            var theme: Theme = itemView.context.getTheme()
+            var tV = TypedValue()
+
             binding.taskTitle.paintFlags = binding.taskTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             binding.taskCheckbox.setImageResource(R.drawable.checked_task)
             binding.taskTitle.setTextColor(ContextCompat.getColor(itemView.context, R.color.md_theme_secondary))
-            binding.taskTime.setTextColor(ContextCompat.getColor(itemView.context, R.color.md_theme_secondary))
-            binding.taskTimeContainer.setCardBackgroundColor(ContextCompat.getColor(itemView.context, R.color.md_theme_secondaryContainer))
 
+            var colorSecondary = theme.resolveAttribute(Rmat.attr.colorSecondary, tV,
+                true)
+            var taskTimeColor: Int = tV.data
+            binding.taskTime.setTextColor(taskTimeColor)
+
+            var colorOnSecondary = theme.resolveAttribute(Rmat.attr.colorOnSecondary, tV,
+                true)
+            var containerColor: Int = tV.data
+            binding.taskTimeContainer.setCardBackgroundColor(containerColor)
 
         }
     }
