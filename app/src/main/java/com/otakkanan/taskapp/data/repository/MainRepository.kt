a8ad2632b32ff34848af9805f82dc11110770053
FasihLisan <fasihullisan091966@gmail.com>
2024-10-16 11:50:28 +0700

    [Update] cahnge month,year event calender

--- app/src/main/java/com/otakkanan/taskapp/data/repository/MainRepository.kt
+++ app/src/main/java/com/otakkanan/taskapp/data/repository/MainRepository.kt
@@ -7,20 +7,28 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.Calendar
 
 class MainRepository {
-
     companion object {
+        val c = Calendar.getInstance()
+        val year: Int = c.get(Calendar.YEAR)
+        val month: Int = c.get(Calendar.MONTH)+1
+
         private val taskDay = listOf(
-            TaskDay(title = "Membuat Dashboard UI", date = LocalDate.of(2024,8,21), time=
+            TaskDay(title = "Membuat Dashboard UI", date = LocalDate.of(year, month,21), time=
             LocalTime.of(10,0,
                 0),
                 isDone =
             true, priority = 2),
-            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(2024,8,21),time= LocalTime.of(11,0,0),isDone = true),
-            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(2024,8,21),time= LocalTime.of(13,0,0),
+            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(year, month,21),time= LocalTime.of
+                (11,
+                0,0),isDone = true),
+            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(year, month,21),time=
+            LocalTime.of
+                (13,0,0),
             isDone = false),
-            TaskDay(title ="Desain UI",date = LocalDate.of(2024,8,21),time= LocalTime.of(17,0,0)
+            TaskDay(title ="Desain UI",date = LocalDate.of(year, month,21),time= LocalTime.of(17,0,0)
             ,isDone = false,
                 subtugas = listOf(
                     SubTugasTaskDay(title = "Membuat moodboard", isDone = true),
@@ -28,26 +36,28 @@ class MainRepository {
                     SubTugasTaskDay(title = "Membuat komponen desain", isDone = false),
                 )
             ),
-            TaskDay(title ="Tugas Laravel",date = LocalDate.of(2024,8,21),time= LocalTime.of(17,0,0)
+            TaskDay(title ="Tugas Laravel",date = LocalDate.of(year, month,21),time= LocalTime.of(17,
+                0,0)
             ,isDone = false),
 
 
-            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(2024,8,2),time= LocalTime.of
+            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(year, month,2),time= LocalTime.of
                 (11,0,0),isDone = true),
-            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(2024,8,2),time= LocalTime
+            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(year, month,2),time= LocalTime
                 .of(13,0,0),
             isDone = false),
-            TaskDay(title ="Tugas Laravel",date = LocalDate.of(2024,8,2),time= LocalTime.of(17,0,0)
+            TaskDay(title ="Tugas Laravel",date = LocalDate.of(year, month,2),time= LocalTime.of(17,
+                0,0)
             ,isDone = false),
 
 
-            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(2024,8,3),time= LocalTime.of
+            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(year, month,3),time= LocalTime.of
                 (11,0,0),isDone = true),
-            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(2024,8,3),time= LocalTime
+            TaskDay(title ="Mempelajari Wireframe",date = LocalDate.of(year, month,3),time= LocalTime
                 .of(13,0,0),
             isDone = false),
 
-            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(2024,8,17),time= LocalTime.of
+            TaskDay(title ="Mengerjakan Kuis",date = LocalDate.of(year, month,17),time= LocalTime.of
                 (11,0,0),isDone = true),
 
 
