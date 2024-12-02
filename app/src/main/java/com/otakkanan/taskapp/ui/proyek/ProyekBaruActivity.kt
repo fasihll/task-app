d4b04f5d40c331827da14247d9927294a362c904
FasihLisan <fasihullisan091966@gmail.com>
2024-12-02 09:06:44 +0700

    [Update] Add dialog Tambah tugas on proyek baru page

--- app/src/main/java/com/otakkanan/taskapp/ui/proyek/ProyekBaruActivity.kt
+++ app/src/main/java/com/otakkanan/taskapp/ui/proyek/ProyekBaruActivity.kt
@@ -7,7 +7,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +20,8 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.checkbox.MaterialCheckBox
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kizitonwose.calendarview.CalendarView
@@ -27,15 +32,17 @@ import com.kizitonwose.calendarview.ui.DayBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
 import com.otakkanan.taskapp.R
 import com.otakkanan.taskapp.component.DatePicker
+import com.otakkanan.taskapp.data.model.Task
 import com.otakkanan.taskapp.databinding.ActivityAddGoalsBinding
 import com.otakkanan.taskapp.databinding.ActivityProyekBaruBinding
 import com.otakkanan.taskapp.databinding.ActivityTugasBerulangBaruBinding
 import com.otakkanan.taskapp.databinding.CalendarDayLayoutBinding
 import com.otakkanan.taskapp.databinding.DialogPengingatBinding
+import com.otakkanan.taskapp.ui.main.beranda.anggota.AnggotaActivity
+import com.otakkanan.taskapp.ui.main.beranda.detail_task.AnggotaAdapter
 import com.otakkanan.taskapp.ui.main.kalender.CalendarAdapter
 import com.otakkanan.taskapp.ui.main.kalender.CalendarHeaderAdapter
 import com.otakkanan.taskapp.ui.main.kalender.DataAdapter
-import com.otakkanan.taskapp.ui.proyek.detail.ProyekTimDetailActivity
 import com.otakkanan.taskapp.utils.Helper
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
@@ -76,13 +83,22 @@ class ProyekBaruActivity : AppCompatActivity(),
                 insets
             }
 
+            val jsonString = Helper.loadJSONFromAssets(this@ProyekBaruActivity, "task_dummy.json")
+
+            // Mengonversi JSON ke List of Goals
+            val taskList: List<Task> = Helper.parseTaskJson(jsonString)
+            val task: Task? = taskList[0]
+
             with(binding){
                 btnBack.setOnClickListener{
                     finish()
                 }
                 btnSelesai.setOnClickListener{
                     finish()
-                    startActivity(Intent(this@ProyekBaruActivity,ProyekTimDetailActivity::class.java))
+                    startActivity(Intent(this@ProyekBaruActivity,ProyekBaruActivity::class.java))
+                }
+                btnTambahTugas.setOnClickListener{
+                    setupDialogAddTugas(task)
                 }
             }
 
@@ -94,6 +110,130 @@ class ProyekBaruActivity : AppCompatActivity(),
             setupTanggalSelesai()
             setupTanggalMulai()
         }
+
+        private fun setupDialogAddTugas(task: Task?) {
+            val dialogView = layoutInflater.inflate(R.layout.proyek_tim_add_tugas_dialog, null)
+
+            val dialog = MaterialAlertDialogBuilder(this@ProyekBaruActivity)
+                .setView(dialogView)
+                .show()
+
+            val edNamaTugas = dialogView.findViewById<EditText>(R.id.ed_nama_tugas)
+            val rbInterval = dialogView.findViewById<RadioButton>(R.id.rb_interval_pengukuran)
+            val rbBerlangsung = dialogView.findViewById<RadioButton>(R.id.rb_sedang_berlansung_selesai)
+            val rbMataUang = dialogView.findViewById<RadioButton>(R.id.rb_mata_uang)
+            val rvAnggota = dialogView.findViewById<RecyclerView>(R.id.rv_anggota)
+            val btnSelsai = dialogView.findViewById<TextView>(R.id.btn_dialog_selesai)
+
+
+            val subInterval = dialogView.findViewById<LinearLayout>(R.id.sub_interval_pengukuran)
+            val edIntervalMulai = dialogView.findViewById<EditText>(R.id.ed_interval_mulai)
+            val edIntervalTarget = dialogView.findViewById<EditText>(R.id.ed_interval_target)
+
+
+            val subBerlansung = dialogView.findViewById<LinearLayout>(R.id.sub_berlangsung)
+            val swBerlangsung = dialogView.findViewById<TextView>(R.id.sw_berlangsung)
+            val swSelesai = dialogView.findViewById<TextView>(R.id.sw_selesai)
+
+
+            val subMataUang = dialogView.findViewById<LinearLayout>(R.id.sub_mata_uang)
+            val edMataUangMulai = dialogView.findViewById<EditText>(R.id.ed_matauang_mulai)
+            val edMataUangTarget = dialogView.findViewById<EditText>(R.id.ed_matauang_target)
+
+            btnSelsai.setOnClickListener{
+                dialog.dismiss()
+            }
+
+            val layoutManager = LinearLayoutManager(this@ProyekBaruActivity, LinearLayoutManager
+                .HORIZONTAL,false)
+            rvAnggota.layoutManager = layoutManager
+
+
+            val adapter = AnggotaAdapter { team ->
+                // Handle button click here
+                val intent = Intent(this@ProyekBaruActivity, AnggotaActivity::class.java)
+                intent.putParcelableArrayListExtra(
+                    AnggotaActivity.TAG, ArrayList(task!!.team ?:
+                emptyList()))
+                startActivity(intent)
+    //                val intent = Intent(this@DetailTaskActivity, AnggotaActivity::class.java)
+    //                startActivity(intent)
+            }
+            rvAnggota.adapter = adapter
+            val limitedTeam = task!!.team?.take(2)
+            adapter.submitList(limitedTeam)
+
+
+            rbInterval.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    updateSubLayoutVisibility(
+                        selectedSubLayout = subInterval,
+                        otherSubLayouts = listOf(subBerlansung, subMataUang)
+                    )
+                }
+            }
+
+            rbBerlangsung.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    updateSubLayoutVisibility(
+                        selectedSubLayout = subBerlansung,
+                        otherSubLayouts = listOf(subInterval, subMataUang)
+                    )
+                }
+            }
+
+            rbMataUang.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    updateSubLayoutVisibility(
+                        selectedSubLayout = subMataUang,
+                        otherSubLayouts = listOf(subInterval, subBerlansung)
+                    )
+                }
+            }
+
+
+            swBerlangsung.setOnClickListener {
+                updateSwitchStyles(swBerlangsung, swSelesai)
+            }
+
+
+            swSelesai.setOnClickListener {
+                updateSwitchStyles(swSelesai, swBerlangsung)
+            }
+
+        }
+
+    fun updateSwitchStyles(selectedSwitch: TextView, unselectedSwitch: TextView) {
+        selectedSwitch.setBackgroundResource(R.drawable.selected_berlangsung_background)
+        selectedSwitch.setTextColor(ContextCompat.getColor(selectedSwitch.context, R.color.md_theme_onSecondary))
+
+        unselectedSwitch.setBackgroundResource(R.color.md_theme_primaryFixed)
+        unselectedSwitch.setTextColor(ContextCompat.getColor(unselectedSwitch.context, R.color
+            .md_theme_onSecondaryFixed))
+    }
+
+    fun updateSubLayoutVisibility(
+        selectedSubLayout: LinearLayout,
+        otherSubLayouts: List<LinearLayout>
+    ) {
+
+        selectedSubLayout.visibility = View.VISIBLE
+
+        otherSubLayouts.forEach { layout ->
+            layout.visibility = View.GONE
+            clearEditTexts(layout)
+        }
+    }
+
+
+    fun clearEditTexts(layout: LinearLayout) {
+        for (i in 0 until layout.childCount) {
+            val child = layout.getChildAt(i)
+            if (child is EditText) {
+                child.text.clear()
+            }
+        }
+    }
 
         private fun setupTanggalMulai() {
             val btnTglMulai = binding.btnTglMulai
